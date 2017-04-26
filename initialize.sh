4ec56f398dc03db373e7b353ea49355f118008cf
animeclub admin <admin@animedj.club>
2017-04-26 10:42:35 +0900

    update config and create initialize script

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,7 @@
 # setup docker container
 docker-compose down
 docker-compose up -d --build
+
+# initiallize databases
+docker-compose run --rm web rails db:migrate
+docker-compose run --rm web rails assets:precompile
